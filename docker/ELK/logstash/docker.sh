b6f920e8c070bda17250bcf0cbcf5921479fdf42
AJEX-埃捷国际 <zhisen2.li@gmail.com>
2023-05-29 15:40:43 +0800

    update logstash shell

--- docker/ELK/logstash/docker.sh
+++ docker/ELK/logstash/docker.sh
@@ -1,10 +1,6 @@
 # Docker Build
-docker image build --platform -t zs-logstash:1.2.0 . && \
+docker image build  -t zs-logstash:8.7.1 . && \
 
 # Push to stage registry
-docker image tag ajex-aone-logstash:1.2.0 asia-south2-docker.pkg.dev/middleware-test-339406/images/ajex/ajex-aone-logstash:1.2.0 && \
-docker image push asia-south2-docker.pkg.dev/middleware-test-339406/images/ajex/ajex-aone-logstash:1.2.0
-
-# Push to production registry
-# docker image tag ajex-aone-logstash:1.2.0 asia-south1-docker.pkg.dev/ajex-production/images/ajex/ajex-aone-logstash:dev-1.2.0 && \
-# docker image push asia-south1-docker.pkg.dev/ajex-production/images/ajex/ajex-aone-logstash:dev-1.2.0
+docker image tag zs-logstash:8.7.1 zs-logstash:8.7.1 && \
+docker image push zhisen2/zs-logstash:8.7.1
