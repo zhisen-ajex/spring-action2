51ae920bbac866265a608c7bb71fd16b3ac2f76c
AJEX-埃捷国际 <zhisen2.li@gmail.com>
2023-06-01 14:50:20 +0800

    1 k8s service discovery 2 licensing-service add k8s client

--- licensing-service/src/main/java/com/zs/license/controller/Test.java
+++ licensing-service/src/main/java/com/zs/license/controller/Test.java
@@ -4,12 +4,21 @@ import com.zs.license.config.ServiceConfig;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @Log4j2
 public class Test {
+    @Autowired
+    private DiscoveryClient discoveryClient;
     @Autowired
     ServiceConfig serviceConfig;
     @Value("${spring.datasource.username}")
@@ -18,5 +27,15 @@ public class Test {
     public void test(){
         log.info("pro:"+serviceConfig.getProperty());
         log.info("d:"+d);
+    }
+    @GetMapping("/serviceurl")
+    public Map<String, List<ServiceInstance>> serviceUrl() {
+        Map<String, List<ServiceInstance>> msl = new HashMap<>();
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            List<ServiceInstance> sis = discoveryClient.getInstances(service);
+            msl.put(service, sis);
+        }
+        return msl;
     }
 }
